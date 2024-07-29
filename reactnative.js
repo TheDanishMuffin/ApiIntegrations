a47cc9b692cdead2b1ccb7c178c20b1289ddf7e2
Dinesh Babu <60652261+TheDanishMuffin@users.noreply.github.com>
2024-07-28 20:18:47 -0400

    Create reactnative.js

---
+++ reactnative.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { AppRegistry, Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+export default class App extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.paragraph}>
+                    Testing React.js -dinesh!
+                </Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: Constants.statusBarHeight,
+        backgroundColor: '#ecf0f1',
+    },
+    paragraph: {
+        margin: 24,
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: '#34495e',
+    },
+});
