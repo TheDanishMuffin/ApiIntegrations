31535c3139293024c8d2a56b5a12825406116ba3
Dinesh Babu <60652261+TheDanishMuffin@users.noreply.github.com>
2024-07-30 10:01:30 -0400

    Add files via upload
    moving over organized code from computer files

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, {useEffect} from 'react'
+import useDrivePicker from 'react-google-drive-picker'
+
+function App() {
+
+  const [openPicker, data, authResponse] = useDrivePicker();
+  const handleOpenPicker = () => {
+    openPicker({
+      clientId: "648055946887-4er5sea1ghchnroe19sf761l6dtmme4i.apps.googleusercontent.com",
+      developerKey: "AIzaSyD26iRiz7BCSLbPqlKCqTg4njaKFIV1zrw",
+      viewId: "DOCS",
+      showUploadView: true,
+      showUploadFolder: true,
+      supportDrives: true,
+      multiselect: true,
+    })
+  }
+
+  useEffect(() => {
+    
+    if (data) {
+      data.docs.map((i) => console.log(i))
+    }
+  }, [data])
+
+  return (
+    <div>
+      <button onClick= {() => handleOpenPicker()}>Picky here!</button>
+    </div>
+  );
+}
+
+export default App;
